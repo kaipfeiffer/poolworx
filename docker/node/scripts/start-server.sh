6b0bb89694835f6983b22d488e4a0af4ab671359
Kai Pfeiffer <kp@paecher.com>
2024-01-31 14:39:45 +0100

    wpcli added

--- docker/node/scripts/start-server.sh
+++ docker/node/scripts/start-server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ -f ./package.json ]
 then
-  echo $1
+  echo $1 - ${ENV_EXPOSE_PORT}
   npm run dev -- --host --port $1
 else
     npm create vite@latest . -- --template vue && npm install && npm run dev -- --host
